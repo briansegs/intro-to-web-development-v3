9965e8296078b7e80f949c71db67a8c5c7658662
bsegers <briansegs@gmail.com>
2026-02-22 11:26:55 -0500

    Added for loop test

---
+++ js-experiment/experiment.js
@@ -0,0 +1,13 @@
+const timesToRepeat = 10;
+const character = "🐱";
+
+// Some loop that takes that character and repeats it  that many times
+
+// Write your code here
+let answer = "";
+
+for (let i = 0; i < timesToRepeat; i++) {
+  answer += character;
+}
+
+console.log(answer);
